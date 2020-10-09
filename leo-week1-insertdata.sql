d5e58e3431aed95a3de7186c1da74c9390d6de69
aguileratech <62417769+aguileratech@users.noreply.github.com>
2020-10-08 17:18:15 -0700

    Add files via upload

---
+++ leo-week1-insertdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO student VALUE(1, 'Jack', 'Biology');
+INSERT INTO student VALUE(2, 'Kate', 'Sociology');
+INSERT INTO student VALUE(3, 'Claire', 'English');
+INSERT INTO student VALUE(4, 'Jack', 'Biology');
+INSERT INTO student VALUE(5, 'Mike', 'Comp Sci');
+INSERT INTO student(student_id, name) VALUES(6, 'Leo');
